d1c44203d2d08021152e79d9bf0086562070968d
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-05-01 02:48:01 +0200

    remove automatically generated users viewset

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,8 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
-from my_study_pal.users.views import UserViewSet
-
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
-router.register("users", UserViewSet)
 
 
 app_name = "api"
