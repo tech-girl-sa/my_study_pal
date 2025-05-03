3dd2f39f1f4072aeb8048dbf9e9cbabfb44a1bbc
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-05-03 19:22:56 +0200

    add id to subjects endpoint

--- my_study_pal/subjects/serializers.py
+++ my_study_pal/subjects/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework.serializers import ModelSerializer
-
 from my_study_pal.subjects.models import Subject
+
 
 
 class SubjectSerializer(ModelSerializer):
     class Meta:
         model = Subject
-        fields = ['title', 'description', 'tags']
+        fields = ['id','title', 'description', 'tags']
