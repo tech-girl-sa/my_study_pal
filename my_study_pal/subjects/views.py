8ca58b761fada60e5051fdf1439b0bd70c47455c
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-05-03 15:24:15 +0200

    fix tags filter and make it show in swagger

--- my_study_pal/subjects/views.py
+++ my_study_pal/subjects/views.py
@@ -1,9 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from my_study_pal.subjects.models import Subject
 from my_study_pal.subjects.serializers import SubjectSerializer
 from django_filters import rest_framework as dfilters
+from django.db.models import Q
 
 
 
@@ -11,8 +14,12 @@ class ArrayContainsFilter(dfilters.Filter):
 
     def filter(self, qs, value):
         if value:
+            items = value.split(",")
             lookup = f"{self.field_name}__contains"
-            return qs.filter(**{lookup:[value]})
+            query = Q(**{lookup:[items[0]]})
+            for item in items[1:]:
+                query = query| Q(**{lookup:[item]})
+            qs=qs.filter(query)
         return qs
 
 
@@ -42,4 +49,16 @@ class SubjectsViewset(ModelViewSet):
     def perform_destroy(self, instance):
         instance.is_archived = True
         instance.save()
+
+    @swagger_auto_schema(
+        operation_description="Filter subjects by tags",
+        manual_parameters=[
+            openapi.Parameter(
+                'tags', openapi.IN_QUERY, description="Comma-separated list of tags",
+                type=openapi.TYPE_STRING
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
