7d15ddbaf9e0e6853657bbb36fb35d08dd819797
tech-girl-sa <mabrouksarra3@gmail.com>
2025-10-05 23:09:03 +0100

    return document and subject titles instead of ids

--- my_study_pal/courses/serializers.py
+++ my_study_pal/courses/serializers.py
@@ -1,12 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField, CharField
 
 from my_study_pal.courses.models import Course, Section, Message
 
 
 class CourseSerializer(ModelSerializer):
+    first_section_id = SerializerMethodField()
+    subject = CharField(source="subject.title", read_only=True)
+    document = CharField(source="document.title", read_only=True)
+
+    def get_first_section_id(self, obj):
+        first_section = obj.sections.first()
+        return first_section.id if first_section else None
+
     class Meta:
         model = Course
-        fields = ['id','title', 'description', 'tags', 'subject', 'document']
+        fields = ['id','title', 'description', 'tags', 'subject', 'document', 'created_at', 'first_section_id']
         read_only_fields = ['document']
 
 
