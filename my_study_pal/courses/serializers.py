31fbe9f03f1db5d44c4709311c807454a2fa33f0
tech-girl-sa <mabrouksarra3@gmail.com>
2025-10-06 19:08:23 +0100

    add document id to course

--- my_study_pal/courses/serializers.py
+++ my_study_pal/courses/serializers.py
@@ -6,7 +6,7 @@ from my_study_pal.courses.models import Course, Section, Message
 class CourseSerializer(ModelSerializer):
     first_section_id = SerializerMethodField()
     subject = CharField(source="subject.title", read_only=True)
-    document = CharField(source="document.title", read_only=True)
+    document_title = CharField(source="document.title", read_only=True)
 
     def get_first_section_id(self, obj):
         first_section = obj.sections.first()
@@ -14,7 +14,8 @@ class CourseSerializer(ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['id','title', 'description', 'tags', 'subject', 'document', 'created_at', 'first_section_id']
+        fields = ['id','title', 'description', 'tags', 'subject', 'document', 'document_title',
+                  'created_at', 'first_section_id']
         read_only_fields = ['document']
 
 
