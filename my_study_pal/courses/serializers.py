97d0ba0b57eb34e2d5611ad207f95ddaa12172bb
tech-girl-sa <mabrouksarra3@gmail.com>
2025-10-16 11:55:17 +0100

    create a default section when getting courses with no sections

--- my_study_pal/courses/serializers.py
+++ my_study_pal/courses/serializers.py
@@ -5,16 +5,20 @@ from my_study_pal.courses.models import Course, Section, Message
 
 class CourseSerializer(ModelSerializer):
     first_section_id = SerializerMethodField()
-    subject = CharField(source="subject.title", read_only=True)
+    subject_title = CharField(source="subject.title", read_only=True)
     document_title = CharField(source="document.title", read_only=True)
 
     def get_first_section_id(self, obj):
         first_section = obj.sections.first()
-        return first_section.id if first_section else None
+        if not first_section:
+            section= Section(title="Main Section", description="Main section for the course", course=obj )
+            section.save()
+            return section.id
+        return first_section.id
 
     class Meta:
         model = Course
-        fields = ['id','title', 'description', 'tags', 'subject', 'document', 'document_title',
+        fields = ['id','title', 'description', 'tags', 'subject_title','subject', 'document', 'document_title',
                   'created_at', 'first_section_id']
         read_only_fields = ['document']
 
